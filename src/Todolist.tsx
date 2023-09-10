bf011786ffca44f7f345a34a8cf3661669d33b0a
Belom7 <Still07pkm@gmail.com>
2023-09-10 16:50:16 +0300

    add: added visualization of the pressed button

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,5 +1,7 @@
 import React, {ChangeEvent, useState} from 'react';
 import {FilterValuesType} from './App';
+import styles from './Todolist.module.css';
+
 
 type TaskType = {
   id: string
@@ -19,29 +21,41 @@ type PropsType = {
 export function Todolist(props: PropsType) {
 
   const [title, setTitle] = useState('')
+  const [error, setError] = useState<string | null>(null)
+  const [buttonName, setButtonName] = useState('all')
 
 
   const onChangeHandler = (value: string) => {
+    setError(null)
     setTitle(value)
   }
   const onClickHandler = () => {
     if (title.trim() !== '') {
-    props.addTask(title)
-    setTitle('')
-  }}
+      props.addTask(title)
+      setTitle('')
+    } else {
+      setError('Title is required')
+    }
+  }
   const onKeyPressHandler = (key: string) => {
     if (key === 'Enter') {
       onClickHandler()
     }
   }
-  const onAllClickHandler = () => {
-    props.changeFilter("all")
-  }
-  const onActiveClickHandler = () => {
-    props.changeFilter("active")
-  }
-  const onCompletedClickHandler = () => {
-    props.changeFilter("completed")
+
+  // const onAllClickHandler = () => {
+  //   props.changeFilter("all")
+  // }
+  // const onActiveClickHandler = () => {
+  //   props.changeFilter("active")
+  // }
+  // const onCompletedClickHandler = () => {
+  //   props.changeFilter("completed")
+  // }
+
+  const tsarHandler = (value:FilterValuesType) => {
+    props.changeFilter(value)
+    setButtonName(value)
   }
   const onChangeIsDoneHandler = (isDone: boolean, id: string) => {
     props.changeIsDone(isDone, id)
@@ -50,12 +64,14 @@ export function Todolist(props: PropsType) {
   return <div>
     <h3>{props.title}</h3>
     <div>
-      <input value={title}
+      <input className={error ? styles.error : ''}
+             value={title}
              onChange={(e) => onChangeHandler(e.currentTarget.value)}
              onKeyPress={(e) => onKeyPressHandler(e.key)}
       />
       <button onClick={onClickHandler}>+</button>
     </div>
+    {error? <div className={styles.errorMessage}>{error}</div> : <></>}
     <ul>
       {
         props.tasks.map(t => <li key={t.id}>
@@ -73,9 +89,9 @@ export function Todolist(props: PropsType) {
       }
     </ul>
     <div>
-      <button onClick={onAllClickHandler}>All</button>
-      <button onClick={onActiveClickHandler}>Active</button>
-      <button onClick={onCompletedClickHandler}>Completed</button>
+      <button className={buttonName === 'all'? styles.activeFilter : ''} onClick={()=>tsarHandler('all')}>All</button>
+      <button className={buttonName === 'active'? styles.activeFilter : ''} onClick={()=>tsarHandler('active')}>Active</button>
+      <button className={buttonName === 'completed'? styles.activeFilter : ''} onClick={()=>tsarHandler('completed')}>Completed</button>
     </div>
   </div>
 }
