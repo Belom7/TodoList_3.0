5115096e998692c8e007de690fa1f3f5b6501c71
Belom7 <Still07pkm@gmail.com>
2023-09-20 21:11:43 +0300

    add: added typing

--- src/api/todoLists-api.ts
+++ src/api/todoLists-api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1/',
   withCredentials: true
@@ -8,15 +7,31 @@ const instance = axios.create({
 
 export const todoListsApi = {
   getTodoList() {
-    return instance.get('todo-lists')
+    return instance.get<todoListType[]>('todo-lists')
   },
   createTodoList(title: string) {
-    return instance.post('todo-lists', {title})
+    return instance.post<ResponseType<{item: todoListType}>>('todo-lists', {title})
   },
   deleteTodoList(todoListId: string) {
-    return instance.delete(`todo-lists/${todoListId}`)
+    return instance.delete<ResponseType>(`todo-lists/${todoListId}`)
   },
   updateTodoList(todoListId: string, title: string) {
-    return instance.put(`/todo-lists/${todoListId}`, {title})
+    return instance.put<ResponseType>(`/todo-lists/${todoListId}`, {title})
   }
+}
+
+type todoListType = {
+  "id": string,
+  "title": string,
+  "addedDate": Date,
+  "order": number
+}
+
+type ResponseType <T={}>= {
+  "data": {
+    "item": T
+  },
+  "messages": string[],
+  "fieldsErrors": [],
+  "resultCode": number
 }
