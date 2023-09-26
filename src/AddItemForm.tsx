31eee6c5b4fff72adcdacfb73f094abe8dc40958
Belom7 <Still07pkm@gmail.com>
2023-09-26 19:55:03 +0300

    add: added disabled button

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -2,13 +2,16 @@ import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
 import { AddBox } from '@mui/icons-material';
+import {RequestStatusType} from "./state/app-reducer";
 
 type AddItemFormPropsType = {
     addItem: (title: string) => void
+    entityStatus?:RequestStatusType
 }
 
 export const AddItemForm = React.memo(function (props: AddItemFormPropsType) {
-    console.log('AddItemForm called')
+
+
 
     let [title, setTitle] = useState('')
     let [error, setError] = useState<string | null>(null)
@@ -43,8 +46,9 @@ export const AddItemForm = React.memo(function (props: AddItemFormPropsType) {
                    onKeyPress={onKeyPressHandler}
                    label="Title"
                    helperText={error}
+                   disabled={props.entityStatus === 'loading'}
         />
-        <IconButton color="primary" onClick={addItem}>
+        <IconButton color="primary" onClick={addItem} disabled={props.entityStatus === 'loading'}>
             <AddBox/>
         </IconButton>
     </div>
